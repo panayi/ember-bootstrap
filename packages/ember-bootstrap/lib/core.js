89d25d78636ca9d0074cd95416c2e99a078a49c2
panayi <panayi1954@gmail.com>
2013-04-25 17:36:10 +0300

    Fix some more typos

--- packages/ember-bootstrap/lib/core.js
+++ packages/ember-bootstrap/lib/core.js
@@ -1,44 +1,43 @@
 var Bootstrap = window.Bootstrap = Ember.Namespace.create();
 
 Ember.Route.reopen({
-	events: {
-		_modalCloseClick: function(modalView) {
-			this.handleModalActionAndDestroy(modalView, modalView.get('closeAction'));
-		},
+  events: {
+    _modalCloseClick: function(modalView) {
+      this.handleModalActionAndDestroy(modalView, modalView.get('closeAction'));
+    },
 
-		_modalPrimaryClick: function(modalView) {
-			this.handleModalActionAndDestroy(modalView, modalView.get('primaryAction'));
-		},
+    _modalPrimaryClick: function(modalView) {
+      this.handleModalActionAndDestroy(modalView, modalView.get('primaryAction'));
+    },
 
-		_modalSecondaryClick: function(modalView) {
-			this.handleModalActionAndDestroy(modalView, modalView.get('secondaryAction'));
-		}
-	},
-	
-	handleModalActionAndDestroy: function(modalView, action) {
-		if (action) {
-			var passedTarget = action.match(/(.+)\:/);
-			var target = passedTarget ? passedTarget[1] : null;
-			var actionName = passedTarget ? action.replace(passedTarget[0], '') : action;
+    _modalSecondaryClick: function(modalView) {
+      this.handleModalActionAndDestroy(modalView, modalView.get('secondaryAction'));
+    }
+  },
+  
+  handleModalActionAndDestroy: function(modalView, action) {
+    if (action) {
+      var passedTarget = action.match(/(.+)\:/);
+      var target = passedTarget ? passedTarget[1] : null;
+      var actionName = passedTarget ? action.replace(passedTarget[0], '') : action;
 
-			if (target === 'controller') {
-				var controller = modalView.get('controller');
-				Ember.warn("Callback for modal not found in controller", controller.actionName);
+      if (target === 'controller') {
+        var controller = modalView.get('controller');
+        Ember.warn("Callback for modal not found in controller", controller && controller.actionName);
 
-				if (typeof controller.actionName === "function") {
-					controller.actionName(modalView);
-				}
-			} else if (target === "view") {
-				Ember.warn("Callback for modal not found in view", modalView.actionName);
+        if (typeof controller[actionName] === "function") {
+          controller[actionName](modalView);
+        }
+      } else if (target === "view") {
+        Ember.warn("Callback for modal not found in view", modalView.actionName);
 
-				if (typeof modalView.actionName === "function") {
-					modalView.actionName();
-				}
-			} else {
-				this.send(action, modalView);
-			}
-		}
+        if (typeof modalView[actionName] === "function") {
+          modalView[actionName]();
+        }
+      } else {
+        this.send(action, modalView);
+      }
+    }
 
-		modalView.destroy();
-	}
+    modalV
 });
